e69f3f1d9492e0740824dd92d27cd1b13f639705
PANKAJ SINGH KUSHWAHA <145460446+PANKAJ-SINGH-KUSHWAHA@users.noreply.github.com>
2025-08-31 14:53:43 +0530

    fixed the bug related to port in main.py related to the drive link

--- backend/main.py
+++ backend/main.py
@@ -96,3 +96,10 @@ async def search(
             "score": float(1 - dist)
         })
     return {"results": results}
+
+# -------------------
+# Run app on Render
+# -------------------
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
